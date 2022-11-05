093ee37467e7e46dd5853c9325a6e584b5468189
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2022-11-05 11:56:58 -0500

    start

---
+++ Concurrence/Concurrence.Desktop/Concurrence.Desktop/Form1.cs
@@ -0,0 +1,15 @@
+namespace Concurrence.Desktop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
