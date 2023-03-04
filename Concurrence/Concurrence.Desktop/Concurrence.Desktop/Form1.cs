1ca32018fad4d8e0a33a804c12fa98a235d869a9
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2023-03-04 11:37:30 -0500

    15 task

--- Concurrence/Concurrence.Desktop/Concurrence.Desktop/Form1.cs
+++ Concurrence/Concurrence.Desktop/Concurrence.Desktop/Form1.cs
@@ -1,3 +1,5 @@
+using System.Runtime.CompilerServices;
+
 namespace Concurrence.Desktop
 {
     public partial class Form1 : Form
@@ -9,14 +11,23 @@ namespace Concurrence.Desktop
 
         private async void btnStart_Click(object sender, EventArgs e)
         {
-            Thread.Sleep(5000); 
+            Thread.Sleep(5000);
         }
 
         private async void btnStartAsync_Click(object sender, EventArgs e)
         {
+            //Es void, pq estamos ante un event hander (manejador de eventos)
             this.lblProcesing.Visible = true;
-            await Task.Delay(TimeSpan.FromSeconds(5));
+            //con el await le estamos diciendo al hilo principal, que se puede ir a hacer otra
+            // cosa mientras lo del await termina
+            await Wait();
+            MessageBox.Show("pasaron los 5 seg");
             this.lblProcesing.Visible = false;
+        }
+
+        private async Task Wait()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(5));
         }
     }
 }
