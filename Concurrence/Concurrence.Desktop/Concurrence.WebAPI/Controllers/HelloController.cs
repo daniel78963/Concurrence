6ac6a229825e36a87082f62b3d1833f7d1ae24fa
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2023-03-04 12:04:40 -0500

    add project API

---
+++ Concurrence/Concurrence.Desktop/Concurrence.WebAPI/Controllers/HelloController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Concurrence.WebAPI.Controllers
+{
+    [Route("greetings")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet("name")]
+        public ActionResult<string> GetGreetings(string name)
+        {
+            return $"Hello {name}";
+        }
+
+    }
+}
