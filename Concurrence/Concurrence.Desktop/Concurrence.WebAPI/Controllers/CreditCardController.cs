e0f0ee679fb67306e60f37b008af8add4699b599
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2023-07-29 11:47:06 -0500

    add RandomGen

--- Concurrence/Concurrence.Desktop/Concurrence.WebAPI/Controllers/CreditCardController.cs
+++ Concurrence/Concurrence.Desktop/Concurrence.WebAPI/Controllers/CreditCardController.cs
@@ -1,6 +1,15 @@
-﻿namespace Concurrence.WebAPI.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Concurrence.WebAPI.Controllers
 {
+    [ApiController]
+    [Route("creditcards")]
     public class CreditCardController
     {
+        [HttpPost]
+        public async Task<ActionResult> PorcessCreditCard([FromBody] string card)
+        {
+
+        }
     }
 }
---
+++ Concurrence/Concurrence.Desktop/Concurrence.WebAPI/Helpers/RandomGen.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Cryptography;
+
+namespace Concurrence.WebAPI.Helpers
+{
+    public class RandomGen
+    {
+        private static RNGCryptoServiceProvider _global = new RNGCryptoServiceProvider();
+
+        [ThreadStatic]
+        private static Random _randomLocal;
+
+        public static double NextDouble()
+        {
+            Random inst = _randomLocal;
+            if (inst == null)
+            {
+                byte[] buffer = new byte[4];
+                _global.GetBytes(buffer);
+                _randomLocal = inst = new Random(BitConverter.ToInt32(buffer, 0));
+            }
+            return inst.NextDouble();
+        }
+    }
+}
